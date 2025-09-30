5fdd30c128b43a00741baaeaeb4028b003126d44
laya04 <fer030@stge.org.uk>
2025-09-30 09:24:47 +0000

    attheqyestion5

--- selction/ifStatements.py
+++ selction/ifStatements.py
@@ -1,5 +1,46 @@
-userAge = float(input("Enter your age:"))
-if userAge >18: 
-    print("You are eliglible to watch the movie")
-elif userAge <18:
-    print("You are not eliglible to watch the movie")
+# userAge = float(input("Enter your age:"))
+# if userAge >18: 
+#     print("You are eliglible to watch the movie")
+# elif userAge <18:
+#     print("You are not eliglible to watch the movie")
+
+#Question 2
+
+# userAge = float(input("Enter your age:"))
+# if userAge >= 18: 
+#     print("You are eliglible to watch an 18+ movie")
+# elif userAge >= 15:
+#     print("You are eliglible to watch the 15 movie")
+# else:
+#     print("You cant watch 18 or 15 movies")
+
+#Queston 3
+# mood = input("Enter your current mood: ")
+# if mood == "happy":
+#     print("Hope your day continues the same!!")
+# elif mood == "sad":
+#     print("Hope your day gets better!!")
+# elif mood == "moody":
+#     print("Hope your mood gets better!!")
+
+#Question4
+# genre = input("Enter your favourite genre (fantasy/crime/romance): ")
+# if genre == "fantasy":
+#     print(""" You could watch: Harry Potter or Christmas Chronicles """)
+# elif genre == "crime":
+#     print(""" You could watch: shinnings or sherlock holmes """)
+# elif genre == "romance":
+#     print(""" You could watch: the summer I turned pretty or 10 things I hate about you""")
+#Question5
+# number1 = float(input("Enter a number:"))
+# number2 = float(input("Enter another number:"))
+# operation = input("Enter your chosen operation (add/subtract/division/multiply): ")
+# if operation == "add":
+#     print(number1 + number2)
+# elif operation == "subtract":
+#     print(number1 - number2)
+# elif operation == "multiply":
+#     print(number1*number2)
+# elif operation == "divide":
+#     print(number1/number2)
+
--- selction/logic.py
+++ selction/logic.py
@@ -1,7 +1,45 @@
-weather = input("Enter weater (sunny/rainy)")
-timeOfDay = input("Enter time (day/night)")
+# weather = input("Enter weater (sunny/rainy)")
+# timeOfDay = input("Enter time (day/night)")
 
-if weather == "sunny" and timeOfDay == "day":
-    print("No need umbrella")
-elif weather == "rain" and (timeOfDay == "day" or timeOfDay =="night"):
-    print(" Take a umbrella")
+# if weather == "sunny" and timeOfDay == "day":
+#     print("No need umbrella")
+# elif weather == "rain" and (timeOfDay == "day" or timeOfDay =="night"):
+#     print(" Take a umbrella")
+
+# #Question2
+# weather = input("Enter weater (sunny/rainy)")
+# temperature = float(input("Enter a a temperature"))
+
+# if weather == "rainy" and temperature > 15:
+#     print(" the temperature is raining and above 15")
+# elif weather == "rainy" and temperature < 15:
+#     print(" Take a umbrella as the temperature is below 15 or DO NOT GO OUTSIDE")
+# else:
+#     print("Stay inside")
+
+#question3
+# appleAmount = int(input("Enter your amount of apples: "))
+# if appleAmount <10:
+#     print(" buy more apples")
+# if appleAmount >= 10:
+#     print("you have enough apples")
+
+#question4
+# driverLicense = input("Enter if you have a drivers license (no/yes)")
+# userAge = float(input("Enter your age"))
+
+# if driverLicense == "yes" and userAge >18:
+#     print(" you have a drivers license and are above 18!")
+# elif driverLicense == "no" and userAge < 18:
+#     print(" you cannot get a drivers license and you do not have one, well done for obeying the rules!!")
+# else:
+#     print("Go get a drivers license!")
+
+#QUESTION5
+# weather = input("Enter weather (rainy/sunny)")
+# carSpeed = float(input("Enter your car speed: "))
+
+# if weather == "sunny" and carSpeed <=60:
+#     print(" you are driving safely, continue")
+# elif weather == "rainy" and carSpeed >60:
+#     print(" you are driving unsafely")

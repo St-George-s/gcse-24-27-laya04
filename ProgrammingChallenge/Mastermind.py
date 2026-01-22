1c9749dc6e481217f075cacf54ae75afb192e437
laya04 <fer030@stge.org.uk>
2026-01-22 12:04:08 +0000

    done

--- ProgrammingChallenge/Mastermind.py
+++ ProgrammingChallenge/Mastermind.py
@@ -1,11 +1,67 @@
+# import random
+# # RandomNumber = str(random.randint (1000, 9999))
+# RandomNumber = "1234"
+# print(RandomNumber)
+# counter=0
+# userNumber = ""
+# while counter != 4:
+#     userNumber = input("Enter a random number: ")
+#     for index in range(len(RandomNumber)):
+#         for indexTwo in range(len(userNumber)):
+#             if RandomNumber[index] == userNumber[indexTwo]:
+#                 print(RandomNumber[index], "==", userNumber[indexTwo])
+#                 counter = counter + 1
+#                 print(counter, "numbers common")
+
+
+# import random
+# # RandomNumber = str(random.randint (1000, 9999))
+# RandomNumber = "1234"
+# print(RandomNumber)
+# counter=0
+# userNumber = ""
+# while counter != 4:
+#     userNumber = input("Enter a random number: ")
+#     for index in range(RandomNumber[0]):
+#         for indexTwo in range(len(userNumber)):
+#             if RandomNumber[0] == userNumber[indexTwo]:
+#                 counter = counter + 1
+#                 print(counter, "numbers common")
+
+
+# import random
+# RandomNumber = str(random.randint (1000, 9999))
+# counter=0
+# numbersGuessed = []
+# userNumber = ""
+# while counter != 4:
+#     userNumber = input("Enter a random number: ")
+#     for index in range(len(RandomNumber)):
+#         for indexTwo in range(len(userNumber)):
+#             if RandomNumber[index] == userNumber[indexTwo] and userNumber[indexTwo] not in numbersGuessed:
+#                 print(RandomNumber[index], "==", userNumber[indexTwo])
+#                 numbersGuessed.append(userNumber[indexTwo])
+#                 counter = counter + 1
+#                 print(counter, "numbers common")
+#                 print(numbersGuessed)
+# print("You have found the number!")
+
+
 import random
 RandomNumber = str(random.randint (1000, 9999))
-userNumber = input("Enter a random number: ")
-
-for index in range(len(RandomNumber)):
-    for indexTwo in range(len(userNumber)):
-        if RandomNumber[index] == userNumber[indexTwo]:
-            print("found")
-            counter = ()
-
-   
+print(RandomNumber)
+counter=0
+numbersGuessed = []
+userNumber = ""
+guesses=0
+while counter != 4:
+    userNumber = input("Enter a random number: ")
+    guesses=guesses = 1
+    for index in range(len(RandomNumber)):
+        for indexTwo in range(len(userNumber)):
+            if RandomNumber[index] == userNumber[indexTwo] and userNumber[indexTwo] not in numbersGuessed:
+                numbersGuessed.append(userNumber[indexTwo])
+                counter = counter + 1
+                print(counter, "numbers common")
+                print(numbersGuessed)
+print("You have found the number in", guesses,"guesses" )

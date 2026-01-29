c11cfc297b83b55e4c1209be95bef3bc25c9cb02
laya04 <fer030@stge.org.uk>
2026-01-29 12:02:11 +0000

    done

---
+++ ProgrammingChallenge/hangman.py
@@ -0,0 +1,22 @@
+LetterList =[]
+chances = int(input("Enter a number of chances: "))
+word = input("Enter a word to guess: ")
+userguess = ""
+blankword = ""
+for i in range (len(word)):
+    blankword = blankword + "_"
+print(blankword)
+while chances != 0:
+    while word != blankword:
+        userguess = input("Enter a new letter: ")
+        found = False
+        chances = chances -1
+        print("you have chances", chances, "left")
+        for i in range (len(word)):
+            if word [i] == userguess:
+                found = True
+                #print(i)
+                blankword = blankword [:i]+ userguess + blankword[i+1:]
+        print(blankword)
+
+print("end game")
